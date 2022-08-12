bddc00b6d0dd152bb47b2789b05fa8aab17e05c2
Fazekas Robert <fazekasrobert6@gmail.com>
2022-08-12 21:37:54 +0200

    Add Telescope, refactor

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,7 +1,8 @@
-local packer_path = vim.fn.stdpath('config') .. '/site'                                                                 
-vim.o.packpath = vim.o.packpath .. ',' .. packer_path 
 local fn = vim.fn
+local packer_path = fn.stdpath('config') .. '/site'                                                                 
 local install_path = fn.stdpath('config')..'/site/pack/packer/start/packer.nvim'
+
+vim.o.packpath = vim.o.packpath .. ',' .. packer_path 
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   vim.cmd [[packadd packer.nvim]]
@@ -10,14 +11,14 @@ end
 return require('packer').startup({
     function(use)
         use 'wbthomason/packer.nvim'
-
+        use {
+            'nvim-telescope/telescope.nvim',
+            requires = {{'nvim-lua/plenary.nvim'}}
+        }
         -- Put here other plugins
 
         if packer_bootstrap then
             require('packer').sync()
         end
-    end,
-    config = {
-        package_root = vim.fn.stdpath('config') .. '/site/pack'
-    }
+    end
 })
